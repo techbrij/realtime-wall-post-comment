1213636f75e6400107a1fd5668c8420c0b68203c
techbrij <techbrij@gmail.com>
2013-06-05 01:46:33 +0530

    First Version

---
+++ WallPostByTechBrij/Hubs/PostHub.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.SignalR;
+using WallPostByTechBrij.Models;
+using WebMatrix.WebData;
+
+namespace WallPostByTechBrij.Hubs
+{    
+    public class PostHub : Hub
+    {
+        private string imgFolder = "/Images/profileimages/";
+        private string defaultAvatar = "user.png";
+
+        // GET api/WallPost
+        public void GetPosts()
+        {
+            using (WallEntities db = new WallEntities())
+            {
+                var ret = (from post in db.Posts.ToList()
+                           orderby post.PostedDate descending
+                           select new
+                           {
+                               Message = post.Message,
+                               PostedBy = post.PostedBy,
+                               PostedByName = post.UserProfile.UserName,
+                               PostedByAvatar = imgFolder + (String.IsNullOrEmpty(post.UserProfile.AvatarExt) ? defaultAvatar : post.PostedBy + "." + post.UserProfile.AvatarExt),
+                               PostedDate = post.PostedDate,
+                               PostId = post.PostId,
+                               PostComments = from comment in post.PostComments.ToList()
+                                              orderby comment.CommentedDate
+                                              select new
+                                              {
+                                                  CommentedBy = comment.CommentedBy,
+                                                  CommentedByName = comment.UserProfile.UserName,
+                                                  CommentedByAvatar = imgFolder + (String.IsNullOrEmpty(comment.UserProfile.AvatarExt) ? defaultAvatar : comment.CommentedBy + "." + comment.UserProfile.AvatarExt),
+                                                  CommentedDate = comment.CommentedDate,
+                                                  CommentId = comment.CommentId,
+                                                  Message = comment.Message,
+                                                  PostId = comment.PostId
+
+                                              }
+                           }).ToArray();
+                Clients.All.loadPosts(ret);
+            }
+        }
+
+        public void AddPost(Post post)
+        {
+            post.PostedBy = WebSecurity.CurrentUserId;
+            post.PostedDate = DateTime.UtcNow;
+            using (WallEntities db = new WallEntities())
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                var usr = db.UserProfiles.FirstOrDefault(x => x.UserId == post.PostedBy);
+                var ret = new
+                {
+                    Message = post.Message,
+                    PostedBy = post.PostedBy,
+                    PostedByName = usr.UserName,
+                    PostedByAvatar = imgFolder + (String.IsNullOrEmpty(usr.AvatarExt) ? defaultAvatar : post.PostedBy + "." + post.UserProfile.AvatarExt),
+                    PostedDate = post.PostedDate,
+                    PostId = post.PostId
+                };
+
+                Clients.Caller.addPost(ret);
+                Clients.Others.newPost(ret);
+            }
+        }
+
+        public dynamic AddComment(PostComment postcomment)
+        {
+            postcomment.CommentedBy = WebSecurity.CurrentUserId;
+            postcomment.CommentedDate = DateTime.UtcNow;
+            using (WallEntities db = new WallEntities())
+            {
+                db.PostComments.Add(postcomment);
+                db.SaveChanges();
+                var usr = db.UserProfiles.FirstOrDefault(x => x.UserId == postcomment.CommentedBy);
+                var ret = new
+                {
+                    CommentedBy = postcomment.CommentedBy,
+                    CommentedByName = usr.UserName,
+                    CommentedByAvatar = imgFolder + (String.IsNullOrEmpty(usr.AvatarExt) ? defaultAvatar : postcomment.CommentedBy + "." + postcomment.UserProfile.AvatarExt),
+                    CommentedDate = postcomment.CommentedDate,
+                    CommentId = postcomment.CommentId,
+                    Message = postcomment.Message,
+                    PostId = postcomment.PostId
+                };
+                Clients.Others.newComment(ret, postcomment.PostId);
+                return ret;
+            }
+        }
+
+             
+    }
+}
---
+++ WallPostByTechBrij/Scripts/wallpost.js
@@ -0,0 +1,165 @@
+﻿/*  It's user control for wall operations like to add/display status and comments. 
+*   Developed By: Brij Mohan
+*   Website: http://techbrij.com
+*   Developed On: 29 May 2013
+*  
+*/
+
+
+function getTimeAgo(varDate) {
+    if (varDate) {
+        return $.timeago(varDate.toString().slice(-1) == 'Z' ? varDate : varDate + 'Z');
+    }
+    else {
+        return '';
+    }
+}
+
+
+// Model
+function Post(data, hub) {
+    var self = this;
+    data = data || {};
+    self.PostId = data.PostId;
+    self.Message = ko.observable(data.Message || "");
+    self.PostedBy = data.PostedBy || "";
+    self.PostedByName = data.PostedByName || "";
+    self.PostedByAvatar = data.PostedByAvatar || "";
+    self.PostedDate = getTimeAgo(data.PostedDate);
+    self.error = ko.observable();
+    self.PostComments = ko.observableArray();
+    self.NewComments = ko.observableArray();
+    self.newCommentMessage = ko.observable();   
+    self.hub = hub;
+    self.addComment = function () {
+        self.hub.server.addComment({ "PostId": self.PostId, "Message": self.newCommentMessage() }).done(function (comment) {
+            self.PostComments.push(new Comment(comment));
+            self.newCommentMessage('');
+        }).fail(function (err) {
+            self.error(err);
+        });        
+    }
+
+    self.loadNewComments = function () {      
+        self.PostComments(self.PostComments().concat(self.NewComments()));
+        self.NewComments([]);
+    }
+    self.toggleComment = function (item, event) {
+        $(event.target).next().find('.publishComment').toggle();
+    }   
+     
+
+    if (data.PostComments) {
+        var mappedPosts = $.map(data.PostComments, function (item) { return new Comment(item); });
+        self.PostComments(mappedPosts);
+    }
+  
+}
+
+
+
+function Comment(data) {
+    var self = this;
+    data = data || {};
+
+    // Persisted properties
+    self.CommentId = data.CommentId;
+    self.PostId = data.PostId;
+    self.Message = ko.observable(data.Message || "");
+    self.CommentedBy = data.CommentedBy || "";
+    self.CommentedByAvatar = data.CommentedByAvatar || "";
+    self.CommentedByName = data.CommentedByName || "";
+    self.CommentedDate = getTimeAgo(data.CommentedDate);
+    self.error = ko.observable();
+}
+
+
+
+
+function viewModel() {
+    var self = this;
+    self.posts = ko.observableArray();
+    self.newMessage = ko.observable();
+    self.error = ko.observable();
+
+    //SignalR related
+    self.newPosts = ko.observableArray();
+    // Reference the proxy for the hub.  
+    self.hub = $.connection.postHub;
+
+
+    self.init = function () {
+        self.error(null);
+        self.hub.server.getPosts().fail(function (err) {
+            self.error(err);
+        });
+    }
+
+    self.addPost = function () {
+        self.error(null);
+        self.hub.server.addPost({ "Message": self.newMessage() }).fail(function (err) {
+            self.error(err);
+        });
+    }
+
+    self.loadNewPosts = function () {
+        self.posts(self.newPosts().concat(self.posts()));
+        self.newPosts([]);
+    }
+
+    //functions called by the Hub
+    self.hub.client.loadPosts = function (data) {
+        var mappedPosts = $.map(data, function (item) { return new Post(item, self.hub); });
+        self.posts(mappedPosts);
+    }
+
+    self.hub.client.addPost = function (post) {
+        self.posts.splice(0, 0, new Post(post, self.hub));
+        self.newMessage('');
+    }
+
+    self.hub.client.newPost = function (post) {
+        self.newPosts.splice(0, 0, new Post(post, self.hub));
+    }
+
+    self.hub.client.error = function (err) {
+        self.error(err);
+    }
+
+    self.hub.client.newComment = function (comment, postId) {
+        //check in existing posts
+        var posts = $.grep(self.posts(), function (item) {
+            return item.PostId === postId;
+        });
+        if (posts.length > 0) {
+            posts[0].NewComments.push(new Comment(comment));
+        }
+        else {
+            //check in new posts (not displayed yet)
+            posts = $.grep(self.newPosts(), function (item) {
+                return item.PostId === postId;
+            });
+            if (posts.length > 0) {
+                posts[0].NewComments.push(new Comment(comment));
+            }
+        }
+    }
+    return self;
+};
+
+//custom bindings
+//textarea autosize
+ko.bindingHandlers.jqAutoresize = {
+    init: function (element, valueAccessor, aBA, vm) {
+        if (!$(element).hasClass('msgTextArea')) {
+            $(element).css('height', '1em');
+        }
+        $(element).autosize();
+    }
+};
+
+var vmPost = new viewModel();
+ko.applyBindings(vmPost);
+$.connection.hub.start().done(function () {
+    vmPost.init();
+});
